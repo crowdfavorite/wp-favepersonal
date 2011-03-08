fdac3b617499db8aa70f6f7b0bb41ae21efda951
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-03-08 20:10:50 +0000

    external for carrington-core 3.1
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@34433 546ca710-db30-0410-8887-d9dd8e66aff0

--- excerpt/excerpt-default.php
+++ excerpt/excerpt-default.php
@@ -26,7 +26,11 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 		<h1 class="post-title"><a href="<?php the_permalink() ?>" title="Permanent link to <?php the_title_attribute() ?>" rel="bookmark" rev="post-<?php the_ID(); ?>"><?php the_title() ?></a></h1>
 		<p class="post-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i'); ?>"><?php the_date(); ?></abbr></p>
 	</div>
-	<div class="post-content">
-		<?php comments_popup_link(__('No Comments', 'carrington-personal'), __('1 Comment', 'carrington-text'), __('% Comments', 'carrington-personal')); ?>		
+	<div class="post-meta">
+		<h5><?php _e('Replies', 'carrington-personal'); ?></h5>
+		<p><?php comments_popup_link(__('No Comments', 'carrington-personal'), __('1 Comment', 'carrington-personal'), __('% Comments', 'carrington-personal')); ?></p>
 	</div>
+	<div class="post-content clearfix">
+		<?php the_excerpt(); ?>
+	</div><!--post-content-->
 </div><!-- .excerpt -->
