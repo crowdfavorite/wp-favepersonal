a271533834da5144f08779bb5338471126484147
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-03-14 21:48:41 +0000

    default attachement styling
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@34632 546ca710-db30-0410-8887-d9dd8e66aff0

--- attachment/attachment-default.php
+++ attachment/attachment-default.php
@@ -22,84 +22,55 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 global $post;
 
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
-<head profile="http://gmpg.org/xfn/11">
-	<title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ).$title_description; ?></title>
-	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+get_header();
 
-	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'carrington-text' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
-	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'carrington-text' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
-	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
-	<?php wp_get_archives('type=monthly&format=link'); ?>
-	
-	<link rel="stylesheet" type="text/css" media="screen, print, handheld" href="<?php bloginfo('template_url') ?>/css/carrington-text.css" />
-	
-<?php
-	wp_head(); 
 ?>
-</head>
-
-<body>
 
 <?php
-
 if (have_posts()) : while (have_posts()) : the_post(); 
-
 ?>
 
-<div id="attachment">
+	<div id="attachment" class="clearfix">
+		
+		<div class="attachment-header">
+			<p class="h5"><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment">&larr; back to &#8220;<?php echo get_the_title($post->post_parent); ?>&#8221;</a></p>
+			<h1><?php the_title(); ?></h1>			
+		</div>
+		<div class="attachment-prev">
+		<?php
+		if (cfct_get_adjacent_image_link(true) != '') {
+			previous_image_link();
+		}
+		?>
+		</div>
+		
+		<div class="attachment-content">
+			<div class="center">
+				<a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'large' ); ?></a>
+			</div>
 
-<p class="top"><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment">&larr; back to &#8220;<?php echo get_the_title($post->post_parent); ?>&#8221;</a></p>
+			<?php 
+			if (!empty($post->post_excerpt)) {
+				echo '<div class="attachment-excerpt">',the_excerpt(),'</div>'; // this is the "caption"
+			}
+			the_content();
+			?>			
+		</div>
 
-
-<a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'large' ); ?></a>
-
-<h1><?php the_title(); ?></h1>
-
-<?php 
-
-if ( !empty($post->post_excerpt) ) the_excerpt(); // this is the "caption"
-
-the_content();
-
-?>
-
-<div class="nav">
-
-<?php
-
-if (cfct_get_adjacent_image_link(false) != '') {
-	echo '<span class="next">',next_image_link(),'</span>';
-}
-if (cfct_get_adjacent_image_link(true) != '') {
-	echo '<span class="previous">',previous_image_link(),'</span>';
-}
-	
-?>
-
-</div>
+		<div class="attachment-next">
+		<?php
+		if (cfct_get_adjacent_image_link(false) != '') {
+			next_image_link();
+		}
+		?>
+		</div>
+	</div><!--#attachment-->
 
 <?php endwhile; else: ?>
 
-<p>Sorry, no attachments matched your criteria.</p>
+	<p>Sorry, no attachments matched your criteria.</p>
 
 <?php endif; ?>
 
-		<div class="clear"></div>
-		<div id="footer">
-<?php
-if (cfct_get_option('cfct_credit') == 'yes') {
-?>
-			<p id="developer-link"><?php printf(__('<a href="http://crowdfavorite.com" title="Custom WordPress development, design and backup services." rel="developer designer">%s</a>', 'carrington-text'), 'Carrington Theme by Crowd Favorite'); ?></p>
-<?php
-}
-?>
-			<p id="generator-link"><?php _e('Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a></span> and <span id="theme-link"><a href="http://carringtontheme.com" title="Carrington theme for WordPress">Carrington</a></span>.', 'carrington-text'); ?></p>
-			</div><!--.wrapper-->
-		</div><!--#footer -->
-	</div>
-	<?php wp_footer() ?>
-</body>
-</html>
+
+<?php get_footer(); ?>
