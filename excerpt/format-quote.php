91cff2e6ac79f58fdc0f9e251dca2f5937e2a131
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-03-16 21:25:57 +0000

    Excerpts for more post-formats
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@34776 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ excerpt/format-quote.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @package carrington-personal
+ *
+ * This file is part of the Carrington Personal Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-personal/
+ *
+ * Copyright (c) 2008-2010 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+?>
+<article id="post-excerpt-<?php the_ID() ?>" <?php post_class('excerpt'); ?>>
+	<p class="post-date"><a href="<?php the_permalink(); ?>"><?php echo cfcp_date(); ?></a></p>
+	<div class="post-content">
+		<blockquote>
+			<?php the_excerpt(); ?>
+		</blockquote>
+	</div><!--post-content-->	
+</article><!-- .excerpt -->
