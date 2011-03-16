381726b5793a7ae92b7300557482673d17ea6d10
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-03-16 18:50:34 +0000

    add missing comments.php file for carrington
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@34759 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ comments.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package carrington-personal
+ *
+ * This file is part of the Carrington Personal Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-personal/
+ *
+ * Copyright (c) 2008-2010 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+
+// NOTE: this file is here for compatibility reasons - active templates are in the comments/ dir 
+
+cfct_comments();
+
+?>
