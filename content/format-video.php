73447fd68914b06b9c2379beb81936e3a8699a42
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-03-30 20:57:35 +0000

    Single video post format
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@35298 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ content/format-video.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @package carrington-personal
+ *
+ * This file is part of the Carrington Personal Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-personal/
+ *
+ * Copyright (c) 2008-2010 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+global $previousday;
+$previousday = -1;
+
+?>
+<article id="post-<?php the_ID() ?>" <?php post_class('post cleafix') ?>>
+	<div class="post-header">
+		<h1 class="post-title"><?php the_title() ?></h1>
+		<p class="post-date"><a href="<?php the_permalink(); ?>"><?php echo cfcp_date(); ?></a></p>
+	</div>
+	
+	<div class="post-media">
+		<iframe src="http://player.vimeo.com/video/13588587?color=ff0065" width="710" height="400" frameborder="0"></iframe>
+	</div>
+	
+	<?php cfct_misc('post-meta'); ?>
+	
+	<div class="post-content clearfix">
+		<?php 
+			the_content('<span class="more-link">'.__('Continued...', 'carrington-personal').'</span>'); 
+			$args = array(
+				'before' => '<p class="pages-link">'. __('Pages: ', 'carrington-personal'),
+				'after' => "</p>\n",
+				'next_or_number' => 'number'
+			);
+			wp_link_pages($args);
+		?>
+	</div><!--post-content-->
+</article><!-- .post -->
---
+++ single/single-format-video.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @package carrington-personal
+ *
+ * This file is part of the Carrington Personal Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-personal/
+ *
+ * Copyright (c) 2008-2010 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+get_header();
+
+?>
+
+<div class="col-abc">
+
+	<?php
+
+	cfct_loop();
+	comments_template();
+
+	?>
+
+</div>
+
+
+<?php get_footer(); ?>
