ff45fbfaa5f82b7ae6da5f97568c755bce6936bd
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-04-26 22:19:33 +0000

    Fix type, un-bold all-caps titles
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@35947 546ca710-db30-0410-8887-d9dd8e66aff0

--- misc/post-meta-excerpts.php
+++ misc/post-meta-excerpts.php
@@ -19,5 +19,5 @@
 /* Using as an include since it's used in many places */
 ?>
 <div class="post-meta">
-	<p class="h5"><?php comments_popup_link(__('No Replies', 'carrington-personal'), __('1 Reply', 'carrington-personal'), __('% Relies', 'carrington-personal')); ?></p>
+	<p class="h5"><?php comments_popup_link(__('No Replies', 'carrington-personal'), __('1 Reply', 'carrington-personal'), __('% Replies', 'carrington-personal')); ?></p>
 </div>
