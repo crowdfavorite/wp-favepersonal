5afdb9315dad38b029e0e265be15437e1470939d
sparker <sparker@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-04-29 15:57:56 +0000

    not including images div if no images defined
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@36046 546ca710-db30-0410-8887-d9dd8e66aff0

--- functions/about/widget/view.php
+++ functions/about/widget/view.php
@@ -1,14 +1,16 @@
 <div class="bio-box-gallery"<?php echo (count($settings['images']) > 1 ? ' id="bio-carousel"' : ''); ?>>
-	<div class="bio-box-gallery-images">
 <?php
 	if (!empty($settings['images'])) {
+		echo '
+			<div class="bio-box-gallery-images">';
 		$thumb_size = 'small-img';
 		foreach ($settings['images'] as $img_id) {
 			echo wp_get_attachment_image($img_id, $thumb_size, false, array());
 		}
+		echo '
+			</div>';
 	}
 ?>
-	</div>
 <?php
 	if (count($settings['images']) > 1) {
 		echo '
@@ -26,6 +28,12 @@
 	<?php
 		if (!empty($settings['links'])) {
 			foreach ($settings['links'] as $link) {
+				if (empty($link['url']) || empty($link['title'])) {
+					continue;
+				}
+				if (empty($link['favicon'])) {
+					$link['favicon'] = 'default';
+				}
 				echo '<li><a href="'.esc_url($link['url']).'" title="'.esc_attr($link['title']).'">'.
 					'<img width="16" height="16" alt="'.esc_attr($link['title']).'" src="'.cf_about_favicon_url($link['favicon']).'" /></a></li>'.PHP_EOL;
 			}
