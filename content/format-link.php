344b1439928002a8d667a7b30c6e7afdea5ced9f
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-04-29 01:53:14 +0000

    Adding post-thumbnail for link screen shots.
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@36032 546ca710-db30-0410-8887-d9dd8e66aff0

--- content/format-link.php
+++ content/format-link.php
@@ -20,17 +20,21 @@
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
+global $previousday;
+$previousday = -1;
+
 ?>
-<article id="post-excerpt-<?php the_ID() ?>" <?php post_class('excerpt'); ?>>
+<article id="post-<?php the_ID() ?>" <?php post_class('post'); ?>>
 	<div class="post-header">
-		<h2 class="post-title"><a href="<?php the_permalink() ?>" title="Permanent link to <?php the_title_attribute() ?>" rel="bookmark" rev="post-<?php the_ID(); ?>"><?php the_title() ?> &rarr;</a></h2>
+		<h1 class="post-title"><a href="<?php the_permalink() ?>" title="Permanent link to <?php the_title_attribute() ?>" rel="bookmark" rev="post-<?php the_ID(); ?>"><?php the_title() ?> &rarr;</a></h1>
 		<p class="post-date"><a href="<?php the_permalink(); ?>"><?php echo cfcp_date(); ?></a></p>
 	</div>
-	<?php cfct_misc('post-meta-excerpts'); ?>
+	<?php cfct_misc('post-meta'); ?>
 	<div class="post-content clearfix">
 		<?php if ( has_post_thumbnail() ) { ?>
 			<a href="<?php the_permalink() ?>" class="link-screenshot"><?php the_post_thumbnail('thumb-img'); ?></a>
 		<?php } ?>
-		<?php the_excerpt(); ?>
+		<?php the_content(); ?>
 	</div><!--post-content-->
-</article><!-- .excerpt -->
+</article><!-- .post -->
+
