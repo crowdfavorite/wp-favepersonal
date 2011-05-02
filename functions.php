2a05021d123a919d91b251dc5454f3f7779185cc
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-05-02 21:13:43 +0000

    fallback to the date format set in the admin
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@36087 546ca710-db30-0410-8887-d9dd8e66aff0

--- functions.php
+++ functions.php
@@ -128,7 +128,8 @@ add_filter( 'excerpt_more', 'cfcp_excerpt_more' );
 // Common date formatting. Uses plugins/cf-compat/
 function cfcp_date() {
 	global $post;
-	return cf_relative_time_ago($post->post_date_gmt, '', 'ago', '4', 'm.d.y', '', true);
+	$date_format = get_option('date_format');
+	return cf_relative_time_ago($post->post_date_gmt, '', 'ago', '4', $date_format, '', true);
 }
 function cfcp_comment_date() {
 	global $comment;
