9b87c0199bbf4ac789537227ea29689e18546875
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-06-03 23:29:17 +0000

    The start of header settings page working HTML
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@37029 546ca710-db30-0410-8887-d9dd8e66aff0

--- functions.php
+++ functions.php
@@ -46,6 +46,7 @@ include_once(CFCT_PATH.'carrington-core/carrington.php');
 include_once(CFCT_PATH.'functions/sidebars.php');
 include_once(CFCT_PATH.'functions/gallery.php');
 include_once(CFCT_PATH.'functions/about/about.php');
+include_once(CFCT_PATH.'functions/header.php');
 include_once(CFCT_PATH.'plugins/load.php');
 
 /**
---
+++ functions/header.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+Plugin Name: Header Settings
+Description: Choose what to display in the header: 3 featured post, header image, or nothing
+Version: dev
+Author: Crowd Favorite
+Author URI: http://crowdfavorite.com
+*/
+
+// This is just the start for working HTML
+	
+// Admin Page
+	function cfcp_header_admin_menu() {
+		add_submenu_page(
+			'themes.php',
+			__('Header', 'carrington-personal'),
+			__('Header', 'carrington-personal'),
+			'manage_options',
+			basename(__FILE__),
+			'cfcp_header_admin_form'
+		);
+	}
+	add_action('admin_menu', 'cfcp_header_admin_menu');
+	
+// Add link to the admin menu bar
+	function cfcp_header_admin_bar() {
+		global $wp_admin_bar;
+		if (current_user_can('manage_options')) {
+			$wp_admin_bar->add_menu(array(
+				'id' => 'cfcp-header',
+				'title' => __('Header', 'cfcp-header'),
+				'href' => admin_url('themes.php?page='.basename(__FILE__)),
+				'parent' => 'appearance'
+			));
+		}
+	}
+	add_action('wp_before_admin_bar_render', 'cfcp_header_admin_bar');
+	
+// The settings form, page content
+	function cfcp_header_admin_form() {
+?>
+
+		<div class="wrap cf cf-clearfix">
+			<h2><?php _e('Header Settings', 'carrington-personal'); ?></h2>
+
+			<div id="cfp-header-settings">
+				<form id="cfcp-header-settings" name="cfcp-header-settings" action="" method="">
+
+
+					<p class="submit"><input class="button button-primary" type="submit" name="submit" value="<?php _e('Save Settings', 'carrington-personal'); ?>" /></p>
+				</form>
+			</div><!--#cfp-header-->
+		</div><!--.cf wrap -->
+
+<?php
+	}
+?>
