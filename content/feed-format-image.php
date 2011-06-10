27f56b3e5fb29100fea6a52711a32ace15ffe951
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-06-10 20:14:11 +0000

    Adding templates for feeds.
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@37177 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ content/feed-format-image.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package carrington-personal
+ *
+ * This file is part of the Carrington Personal Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-personal/
+ *
+ * Copyright (c) 2008-2010 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+?>
+
+<?php remove_filter('the_content', 'cfct_content_feed'); ?>
+
+<p><?php the_post_thumbnail('medium-img'); ?></p>
+<?php the_content_feed('rss2'); ?>
+
+<?php add_filter('the_content', 'cfct_content_feed'); ?>
---
+++ content/feed-format-status.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @package carrington-personal
+ *
+ * This file is part of the Carrington Personal Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/carrington-personal/
+ *
+ * Copyright (c) 2008-2010 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+?>
+
+<?php remove_filter('the_content', 'cfct_content_feed'); ?>
+
+<div style="font-size:1.5em; line-height:1.5em;">
+	<?php the_content_feed('rss2'); ?>
+</div>
+
+<?php add_filter('the_content', 'cfct_content_feed'); ?>
