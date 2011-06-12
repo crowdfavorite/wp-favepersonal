4230e64ee5ab95e0133f3774cda02b6e4b5c214f
aking <aking@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-06-12 21:25:51 +0000

    forgot to add this
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@37202 546ca710-db30-0410-8887-d9dd8e66aff0

---
+++ functions/about/views/link-remove-popover.php
@@ -0,0 +1,12 @@
+<div class="cfp-popover-content">
+	
+<!-- link data -->
+	<p class="title"></p>
+	<p class="url"></p>
+<!-- /link data -->
+	
+</div>
+
+<div class="cfp-popover-footer">
+	<a href="#" class="cf-remove-link"><?php _e('Remove', 'favepersonal'); ?></a>
+</div>
