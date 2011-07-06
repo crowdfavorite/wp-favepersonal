76ac50344bf9a6eb1588724b500e65952f459472
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-07-06 13:15:35 +0000

    Always showing the "upload images" button for gallery format
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@37769 546ca710-db30-0410-8887-d9dd8e66aff0

--- plugins/cf-post-format/views/format-gallery.php
+++ plugins/cf-post-format/views/format-gallery.php
@@ -1,16 +1,10 @@
 <div id="cfpf-format-gallery-preview" class="cp-elm-block cp-elm-block-image" style="display: none;">
 	<label><span><?php _e('Gallery Images', 'cf-post-format'); ?></span></label>
 	<div class="cp-elm-container">
-<?php
 
-if (!cfpf_post_has_gallery()) {
-?>
-		<p class="none"><a href="#" class="button"><?php _e('Upload Images', 'cf-post-format'); ?></a></p>
 <?php
-}
-else {
 	echo do_shortcode('[gallery columns="9999"]');
-}
 ?>
+<p class="none"><a href="#" class="button"><?php _e('Upload Images', 'cf-post-format'); ?></a></p>
 	</div>
 </div>
