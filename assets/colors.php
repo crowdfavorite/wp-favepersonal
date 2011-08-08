2f43ff953cc2f26467f3e8634779ff014e483b08
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-08-08 21:34:09 +0000

    add border color to comments from post-author
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@38638 546ca710-db30-0410-8887-d9dd8e66aff0

--- assets/colors.php
+++ assets/colors.php
@@ -266,6 +266,9 @@ a:hover {
 	color: <?php echo cf_kuler_color('darkest', 'social_nav_a_color'); ?>;
 	background-color: <?php echo cf_kuler_color('darkest', 'social_nav_a_background'); ?>;
 }
+#social .bypostauthor .social-comment-inner {
+	border-bottom-color: <?php echo cf_kuler_color('medium', 'social_comment_inner'); ?>;
+}
 #social .social-posted-from {
 	background-color: <?php echo cf_kuler_color('light', 'social_posted_form_background'); ?>;
 }
