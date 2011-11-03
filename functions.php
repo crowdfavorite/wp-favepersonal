d879ff105f22aaf54abb5d185897df3ecf6cdbe8
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-11-03 14:50:26 +0000

    Starting with HTML5 normalize, setting production to false for development
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/branches/normalize@40809 546ca710-db30-0410-8887-d9dd8e66aff0

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ define('CFCT_DEBUG', false);
  * In production mode, or doing development?
  * When true, assets/load.php will enqueue the built versions of the files
  */
-define('CFCT_PRODUCTION', true);
+define('CFCT_PRODUCTION', false);
 
 
 define('CFCT_PATH', trailingslashit(TEMPLATEPATH));
