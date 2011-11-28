f191067f5604785cffb3472fede105b8ec1236e4
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-11-28 20:25:04 +0000

    calling forms/search.php from searchform.php
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/branches/normalize@41410 546ca710-db30-0410-8887-d9dd8e66aff0

--- searchform.php
+++ searchform.php
@@ -20,17 +20,6 @@
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
-if (get_option('permalink_structure') != '') {
-	$onsubmit = "location.href=this.action+'search/'+encodeURIComponent(this.s.value).replace(/%20/g, '+'); return false;";
-}
-else {
-	$onsubmit = '';
-}
+cfct_form('search');
 
 ?>
-
-<form class="searchform" method="get" action="<?php echo trailingslashit(get_bloginfo('url')); ?>" onsubmit="<?php echo $onsubmit; ?>">
-	<label for="s">Search&hellip;</label>
-	<input type="text" id="s" name="s" value="<?php echo esc_html($s, 1); ?>" size="15" />
-	<input type="submit" id="searchsubmit" value="<?php _e('Search', 'favepersonal'); ?>">
-</form>
