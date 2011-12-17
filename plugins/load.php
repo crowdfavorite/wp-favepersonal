678651a661f4455130367ec1fed5ac695ef9a698
aking <aking@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-12-17 22:18:52 +0000

    no longer need cf-compat
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@41856 546ca710-db30-0410-8887-d9dd8e66aff0

--- plugins/load.php
+++ plugins/load.php
@@ -1,8 +1,5 @@
 <?php
 
-include_once('cf-compat/cf-compat.php');
 include_once('cf-colors/cf-colors.php');
 include_once('cf-post-formats/cf-post-formats.php');
 include_once('cf-archive-title/cf-archive-title.php');
-
-?>
