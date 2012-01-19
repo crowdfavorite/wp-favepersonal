crowdfavorite/wp-favepersonal

commit 04895161edc5ea288146ff3bed818bde0aca71f4

cfcp to cfp for bio widget page, no underscores for items, adding cursor move for actual link items
git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@42539 546ca710-db30-0410-8887-d9dd8e66aff0

functions/about/js/about-admin.js
@@ -145,7 +145,7 @@ jQuery(function($) {
 		});
 
 		// attach actions to icons for delete actions
-		$('.cfcp_about_link_item').popover({
+		$('.cfcp-about-link-item').popover({
 			my: 'left top',
 			at: 'center bottom',
 			offset: '-27px 0',

assets/css/admin.css
@@ -582,6 +582,10 @@ button.cf-btn-delete {
 	vertical-align: middle;
 }
 
+.cfp-about-link-item a {
+	cursor: move;
+}
+
 /** @section Utilities
  -------------------------------------------------- */
 /* Popup Actions */

functions/about/views/admin-view.php
@@ -11,7 +11,7 @@ if (!empty($_GET['settings-updated']) && $_GET['settings-updated'] == true) {
 	<p>Use this settings screen to set your photos, title, bio text and links for the Bio Widget before adding it to a sidebar.</p>
 	
 	<div id="cfp-about-settings">
-		<form id="cfcp-about-settings" name="cfcp-about-settings" action="<?php echo admin_url('options.php'); ?>" method="post">
+		<form id="cfp-about-settings" name="cfp-about-settings" action="<?php echo admin_url('options.php'); ?>" method="post">
 
 			<?php settings_fields(CFCP_ABOUT_SETTINGS); ?>
 			

functions/about/views/link-item.php
@@ -4,7 +4,7 @@
 	// we can do name="settings[links][]" and let the dom order be the order instead of having to
 	// keep track of the items ids and the order during editing
 ?>
-<li class="cfcp_about_link_item" id="link-<?php echo md5(json_encode($link)); ?>">
+<li class="cfp-about-link-item" id="link-<?php echo md5(json_encode($link)); ?>">
 	<a href="<?php echo esc_url($link['url']); ?>"><img src="<?php echo $favicon_url; ?>" width="16" height="16" /></a>
 	<input type="hidden" name="<?php echo CFCP_ABOUT_SETTINGS; ?>[links][]" value='<?php echo json_encode($link); ?>' />
 </li>

commit 9381a4a6cf9da9f6e54a67b5ad3c2102493f00d0

fixing the bug Devin introduced in r42539
git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@42614 546ca710-db30-0410-8887-d9dd8e66aff0

functions/about/js/about-admin.js
@@ -145,7 +145,7 @@ jQuery(function($) {
 		});
 
 		// attach actions to icons for delete actions
-		$('.cfcp-about-link-item').popover({
+		$('.cfp-about-link-item').popover({
 			my: 'left top',
 			at: 'center bottom',
 			offset: '-27px 0',