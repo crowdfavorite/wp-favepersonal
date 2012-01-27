bed5bb99455be4dcdadbbfe26fdd3009fc820576
jcornwall <jcornwall@546ca710-db30-0410-8887-d9dd8e66aff0>
2012-01-27 19:09:57 +0000

    checking formats for featured image in featured post section
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-themes/carrington-personal/trunk@42970 546ca710-db30-0410-8887-d9dd8e66aff0

--- header/featured/init.php
+++ header/featured/init.php
@@ -6,9 +6,16 @@ setup_postdata($post);
 
 $class = array();
 $image = '';
+$format = get_post_format();
+
+if ($format == 'status' || $format == 'quote' || $format == 'link') {
+	$show_featured_image = false;
+} else {
+	$show_featured_image = true;
+}
 
 // check for featured image
-if (has_post_thumbnail()) {
+if (has_post_thumbnail() && ($show_featured_image == true)) {
 // set class
 	$class[] = 'has-featured-img';
 // set image
