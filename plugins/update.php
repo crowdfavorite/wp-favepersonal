cb8dbfda5b398227ae9ec5ce8a57eca0af1e2899
Evan Anderson <ejdanderson@gmail.com>
2012-03-19 19:12:55 -0600

    CF Custom Upgrade feature

---
+++ plugins/update.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @package favepersonal
+ *
+ * This file is part of the FavePersonal Theme for WordPress
+ * http://crowdfavorite.com/wordpress/themes/favepersonal/
+ *
+ * Copyright (c) 2008-2011 Crowd Favorite, Ltd. All rights reserved.
+ * http://crowdfavorite.com
+ *
+ * **********************************************************************
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * **********************************************************************
+ */
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+
+delete_site_transient('update_themes'); // for testing only
+
+function cfcp_pre_set_site_transient_update_themes($value) {
+
+	if (!function_exists('current_theme_info')) {
+		include_once(trailingslashit(ABSPATH).'wp-admin/includes/theme.php');
+	}
+	$theme = current_theme_info();
+	$response = wp_remote_get(
+		'http://api.crowdfavorite.com/wordpress/themes/favepersonal/?cf_action=version-api&request=latest&current_version='.CFCT_THEME_VERSION,
+		array(
+			'timeout' => 20,
+			'httpversion' => '1.1'
+		)
+	);
+	if (is_array($response) && isset($response['response']) && isset($response['response']['code']) && $response['response']['code'] == 200 && !empty($response['body'])) {
+		if (strpos($response['body'], '\\\\') !== false) {
+			$response['body'] = stripslashes($response['body']);
+		}
+		$version = json_decode($response['body']);
+		if (isset($version->current_version) && version_compare(CFCT_THEME_VERSION, $version->current_version) < 0) {
+			$value->response[$theme->template] = array(
+				'new_version' => $version->current_version,
+				'url' => $version->more_info,
+			);
+		}
+	}
+	return $value;
+}
+add_filter('pre_set_site_transient_update_themes', 'cfcp_pre_set_site_transient_update_themes');
+
+?>
