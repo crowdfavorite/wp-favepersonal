0a004db6a2c080470739d4252f9446876263809a
Will Moritz <will@crowdfavorite.com>
2013-01-25 11:18:49 -0700

    titles and image heights

--- excerpt/format-image.php
+++ excerpt/format-image.php
@@ -25,20 +25,11 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 <?php if ( has_post_thumbnail() ) { ?>
 <?php } ?>
-	<div class="entry-content">
-<?php
-if ( has_post_thumbnail() ) {
-?>
-		<div class="entry-media">
-			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium-img'); ?></a>
-<!-- 			<div class="entry-header">
-				<h1 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'favepersonal' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
-				<time class="entry-date" datetime="<?php the_time('c'); ?>" pubdate><a href="<?php the_permalink(); ?>"><?php echo cfcp_date(); ?></a></time>
-			</div> -->
-		</div>
-<?php
-}
-the_excerpt();
-?>
+	<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium-img'); ?></a>
+	<div class="entry-header-img">
+		<h1 class="featured-title entry-title-img"><a href="<?php the_permalink() ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'favepersonal' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
 	</div>
+<?php
+	the_excerpt();
+?>
 </article>
