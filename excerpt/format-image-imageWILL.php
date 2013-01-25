c3b89611054065f45030a8c7b7299c89549c7c40
Will Moritz <will@crowdfavorite.com>
2013-01-25 11:30:08 -0700

    halfway thorugh cfct integration

--- excerpt/format-image-imageWILL.php
+++ excerpt/format-image-imageWILL.php
@@ -21,20 +21,15 @@ if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 ?>
-<article id="post-excerpt-<?php the_ID() ?>" <?php post_class('excerpt clearfix'); ?>>
-	<div class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'favepersonal' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
-		<time class="entry-date" datetime="<?php the_time('c'); ?>" pubdate><a href="<?php the_permalink(); ?>"><?php echo cfcp_date(); ?></a></time>
-	</div>
+<article id="post-excerpt-<?php the_ID() ?>" <?php post_class('excerpt clearfix featured img-archive'); ?>>
+
 <?php if ( has_post_thumbnail() ) { ?>
 <?php } ?>
-	<div class="entry-content">
-		<div class="entry-media">
-			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium-img'); ?></a>
-		</div>
-	<?php
-		the_excerpt();
-	?>
+	<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium-img'); ?></a>
+	<div class="entry-header-img">
+		<h1 class="featured-title entry-title-img"><a href="<?php the_permalink() ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'favepersonal' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
 	</div>
-	<?php cfct_misc('entry-meta-excerpts'); ?>
+<?php
+	the_excerpt();
+?>
 </article>
