38a7ca631ac9d2942be2a42c53ad4a73f1f223a9
Will Moritz <will@crowdfavorite.com>
2013-02-05 15:05:10 -0700

    removed extraneous files

--- excerpt/format-image.php
+++ excerpt/format-image.php
@@ -23,18 +23,20 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 ?>
 <article id="post-excerpt-<?php the_ID() ?>" <?php post_class('excerpt clearfix'); ?>>
 	<div class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'favepersonal' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
+		<h1 class="entry-title"><a href="<?php the_permalink() ?>"  title="<?php printf( esc_attr__( 'Permalink to %s', 'favepersonal' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
 		<time class="entry-date" datetime="<?php the_time('c'); ?>" pubdate><a href="<?php the_permalink(); ?>"><?php echo cfcp_date(); ?></a></time>
 	</div>
-<?php if ( has_post_thumbnail() ) { ?>
-<?php } ?>
 	<div class="entry-content">
+<?php
+if ( has_post_thumbnail() ) {
+?>
 		<div class="entry-media">
-			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium-img'); ?></a>
+			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('banner-img'); ?></a>
 		</div>
-	<?php
-		the_excerpt();
-	?>
+<?php
+}
+the_excerpt();
+?>
 	</div>
 	<?php cfct_misc('entry-meta-excerpts'); ?>
 </article>
