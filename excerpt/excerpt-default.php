5434aee2cded5dbb1c7abbad999a22b86444ff5f
Will Moritz <will@crowdfavorite.com>
2013-02-11 17:13:27 -0700

    background for type-image

--- excerpt/excerpt-default.php
+++ excerpt/excerpt-default.php
@@ -12,7 +12,7 @@
  * **********************************************************************
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * **********************************************************************
  */
 
@@ -28,27 +28,27 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 	<div class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink() ?>"  title="<?php printf( esc_attr__( 'Permalink to %s', 'favepersonal' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
 		<time class="entry-date" datetime="<?php the_time('c'); ?>" pubdate><a href="<?php the_permalink(); ?>"><?php echo cfcp_date(); ?></a></time>
-	</div>	
+	</div>
 	<div class="entry-content">
-<?php 
-if ( has_post_thumbnail() ) { 
+<?php
+if ( has_post_thumbnail() ) {
 ?>
 		<div class="entry-media">
 			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('banner-img'); ?></a>
 		</div>
-<?php 
-} 
+<?php
+}
 the_excerpt();
 ?>
 	</div>
 	<?php cfct_misc('entry-meta-excerpts'); ?>
 </article>
-<?php 
+<?php
 // END Posts that are NOT post_type image
 
 // Posts that ARE post_type image
-} 
-else { 
+}
+else {
 ?>
 <article id="post-excerpt-<?php the_ID() ?>" <?php post_class('excerpt clearfix featured img-archive'); ?>>
 
@@ -61,6 +61,6 @@ else {
 <?php
 	the_excerpt();
 ?>
-	
+
 </article>
 <?php } // end Posts that ARE post_type image ?>
