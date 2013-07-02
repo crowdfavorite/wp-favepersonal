9c34fadc532ed95cd1c6bf6614d1d70f85a56341
Alex King <alex@alexking.org>
2013-07-02 15:52:11 -0600

    adding native fb broadcast plugin

--- plugins/load.php
+++ plugins/load.php
@@ -3,3 +3,4 @@
 include_once('cf-colors/cf-colors.php');
 include_once('cf-post-formats/cf-post-formats.php');
 include_once('cf-archive-title/cf-archive-title.php');
+include_once('social-native-broadcasts/social-native-broadcasts.php');
